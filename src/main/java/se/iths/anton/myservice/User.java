71c89decee59680ba6dd0e4b5f96b67d95116064
Anton Johansson <anton.elis@hotmail.com>
2020-02-26 15:16:22 +0100

    created interface usersrepository

--- src/main/java/se/iths/anton/myservice/User.java
+++ src/main/java/se/iths/anton/myservice/User.java
@@ -1,7 +1,18 @@
 package se.iths.anton.myservice;
 
+
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class User {
-    int id;
+    @Id @Generated int id;
     String userName;
     String realName;
     String city;
