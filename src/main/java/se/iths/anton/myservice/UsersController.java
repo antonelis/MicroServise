af06a9843853481996b43d82c18f61bcf6a852d7
Anton Johansson <anton.elis@hotmail.com>
2020-02-26 15:32:46 +0100

    created UsersModelAssembler

--- src/main/java/se/iths/anton/myservice/UsersController.java
+++ src/main/java/se/iths/anton/myservice/UsersController.java
@@ -1,8 +1,37 @@
 package se.iths.anton.myservice;
 
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/users")
 public class UsersController {
+
+    final UsersRepository repository;
+
+    public UsersController(UsersRepository usersRepository) {
+        this.repository = usersRepository;
+    }
+    @GetMapping
+    public CollectionModel<EntityModel<User>> all() {
+       // log.debug("All persons called");
+        return assembler.toCollectionModel(repository.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<EntityModel<Person>> one(@PathVariable long id) {
+        return repository.findById(id)
+                .map(assembler::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+
 }
---
+++ src/main/java/se/iths/anton/myservice/UsersModelAssembler.java
@@ -0,0 +1,17 @@
+package se.iths.anton.myservice;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+public class UsersModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+    @Override
+    public EntityModel<User> toModel(User entity) {
+        return null;
+    }
+
+    @Override
+    public CollectionModel<EntityModel<User>> toCollectionModel(Iterable<? extends User> entities) {
+        return null;
+    }
+}
