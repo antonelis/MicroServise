9440bcfb3c3d505bc439dc00d07eae3b914b3271
Anton Johansson <anton.elis@hotmail.com>
2020-03-04 08:57:05 +0100

    saving 3 users to database

--- src/main/java/se/iths/anton/myservice/SetupDatabase.java
+++ src/main/java/se/iths/anton/myservice/SetupDatabase.java
@@ -1,6 +1,7 @@
 package se.iths.anton.myservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +10,15 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class SetupDatabase {
     @Bean
-    void initDatabase(UsersRepository repository) {
-
+    CommandLineRunner initDatabase(UsersRepository repository) {
+        return args -> {
+            if( repository.count() == 0) {
+                //New empty database, add some persons
+                repository.save(new User(0,"Anton", "Anton Johansson", "Mölndal", 25000, true));
+                repository.save(new User(0,"Sebbe", "Sebastian Waltilla", "Fjärås", 60000, true));
+                repository.save(new User(0,"Jonte", "Jonathan Holm", "Umeå", 40000, false));
+            }
+        };
     }
     @Bean
     RestTemplate getRestTemplate(){
