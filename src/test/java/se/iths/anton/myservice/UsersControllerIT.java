02f4838de19ffb06454dd0ecaaa33b9d0356a4c3
Anton Johansson <anton.elis@hotmail.com>
2020-03-05 13:45:59 +0100

    created UsersControllerIT with sendRealHttpRequestWithPost test method

---
+++ src/test/java/se/iths/anton/myservice/UsersControllerIT.java
@@ -0,0 +1,39 @@
+package se.iths.anton.myservice;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.ws.rs.core.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsersControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    void sendRealHttpRequestWithPost() throws Exception{
+        User user = new User (0, "testUser", "testName", "testCity", 10, true);
+        mockMvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json("{\"id\":4,\"userName\":\"testUser\",\"realName\":\"testName\",\"city\":\"testCity\",\"income\":10,\"inRelation\":true}"));
+    }
+}
